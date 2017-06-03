13d4e2901429cb0823e227d6212cbeb432ef71f3
Sekazedy <sekazedy@gmail.com>
2017-06-03 11:19:28 +0300

    #2 - added important vars +
    - colors, positions, fps controller and game over function

--- snakeGame.py
+++ snakeGame.py
@@ -2,6 +2,15 @@
 
 import pygame, sys, random, time
 
+def gameOver():
+    my_font = pygame.font.SysFont('monaco', 72)
+    game_over_surface = my_font.render('Game over!', True, red)
+    game_over_rectangle = game_over_surface.get_rect()
+    game_over_rectangle.midtop = (400, 15)  #(x, y)
+    play_surface.blit(game_over_surface, game_over_rectangle)
+    pygame.display.flip()
+
+
 # check for initializing errors
 check_errors = pygame.init()
 if check_errors[1] > 0:
@@ -11,7 +20,28 @@ else:
     print("PyGame successfully initialized!")
 
 # Play surface
-resolution = (800, 600)
-playSurface = pygame.display.set_mode(resolution)
+resolution = (800, 600) # (x, y)
+play_surface = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Snake game!')
-time.sleep(5)
+
+# Colors
+red = pygame.Color(255, 0, 0)   # RGB colors; Gameover
+green = pygame.Color(0, 255, 0) # snake
+black = pygame.Color(0, 0, 0)   # score
+white = pygame.Color(255, 255, 255) # background
+brown = pygame.Color(165, 42, 42)   # snake food
+
+# FPS (frames-per-second) controller
+fps_controller = pygame.time.Clock()
+
+snake_pos = [100, 50]   # x and y of snake head = first block
+snake_body = [[100, 50], [90, 50], [80, 50]]    # snake body at the beginning
+
+food_pos = [random.randrange(1, 80) * 10, random.randrange(1, 60) * 10] # 10 multiplier is important to catch food
+food_spawn = True
+
+direction = 'RIGHT'
+change_to = direction
+
+gameOver()
+time.sleep(10)
